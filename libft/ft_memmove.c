62fbf8ec142c3e44cb47b9e599d776e3b10ccbbb
it-runner <144400811+it-runner@users.noreply.github.com>
2024-12-25 21:09:51 +0100

    Add files via upload

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -14,10 +14,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
 	if (dest == src)
 		return (dest);
-	else if (dest > src)
+	i = 0;
+	if (dest > src)
 	{
 		while (i < n)
 		{
