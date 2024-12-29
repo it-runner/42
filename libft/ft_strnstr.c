561b09c926c22b26f6cd3a28bf7d74962ff030be
it-runner <144400811+it-runner@users.noreply.github.com>
2024-12-29 11:04:13 +0100

    Add files via upload

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -9,9 +9,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if ((!haystack || !needle) && len == 0)
-		return (NULL);
-	if (needle[0] == '\0')
+	if (!needle[0])
 		return ((char *)haystack);
 	i = 0;
 	while (haystack[i] && i < len)
