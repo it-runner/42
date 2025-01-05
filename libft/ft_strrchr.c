63da7c620c99b5318774d848a3cc9346a21f20af
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-05 12:40:58 +0100

    Add files via upload

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -7,18 +7,20 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	const char	*last_occ;
-	size_t		i;
+	const char		*last_occ;
+	size_t			i;
+	unsigned char	uc;
 
 	last_occ = NULL;
 	i = 0;
+	uc = (unsigned char)c;
 	while (str[i])
 	{
-		if (str[i] == (unsigned char)c)
+		if (str[i] == uc)
 			last_occ = &str[i];
-		++i;
+		i++;
 	}
-	if (str[i] == (unsigned char)c)
+	if (str[i] == uc)
 		last_occ = &str[i];
 	return ((char *)last_occ);
 }
