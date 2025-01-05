e9b73b962cdfdbc1fe9e63b7a2f6889904d2c5d4
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-05 12:53:06 +0100

    Add files via upload

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -18,16 +18,20 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
 	i = 0;
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
 	if (n == 0)
 		return (0);
 	while ((i < (n - 1)) && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+			return (us1[i] - us2[i]);
 		i++;
 	}
-	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	return (us1[i] - us2[i]);
 }
