55dc16e1a01d7f02966b43109901a131160e17e6
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-20 17:56:01 +0100

    Add files via upload

--- ft_printf/inc/ft_printf.h
+++ ft_printf/inc/ft_printf.h
@@ -1,1 +1,16 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <stddef.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	print_char(int c);
+int	print_str(char *str);
+int	print_ptr(void *ptr);
+int	print_digit(int n);
+int	print_unsigned(unsigned int n);
+int	print_hex(unsigned int n, char format);
+
+#endif
