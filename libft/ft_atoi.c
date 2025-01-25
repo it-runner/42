2d338196f317156a0d9353a777261081ac65da1a
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-25 14:46:40 +0100

    Add files via upload

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -17,11 +17,8 @@ int	ft_atoi(const char *str)
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-')
-	{
 		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
+	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -8,5 +8,6 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, 0, n);
+	if (n > 0)
+		ft_memset(s, 0, n);
 }
