5cafa18849f1b23f3088c2416ae7d95a9096ffb8
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-27 09:16:32 +0100

    Add files via upload

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -14,6 +14,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*str2;
 	size_t			i;
 
+	if (s1 == s2)
+		return (0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
