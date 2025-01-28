9235bd094c7155194f84ab261496f7a066e28cb3
it-runner <144400811+it-runner@users.noreply.github.com>
2025-01-28 11:57:34 +0100

    Add files via upload

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,39 @@
 	It returns the array of new strings resulting from the split or NULL
 	if the allocation fails.
 */
+
 #include "libft.h"
+
+static int	count_words(const char *str, char c);
+static char	*duplicate_word(const char *str, int start, int end);
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int		index;
+	char	**split;
+
+	split = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!s || !split)
+		return (0);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = duplicate_word(s, index, i);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
+}
 
 static int	count_words(const char *str, char c)
 {
@@ -42,32 +74,4 @@ static char	*duplicate_word(const char *str, int start, int end)
 		word[i++] = str[start++];
 	word[i] = '\0';
 	return (word);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	size_t	i;
-	size_t	j;
-	int		index;
-	char	**split;
-
-	split = malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!s || !split)
-		return (0);
-	i = 0;
-	j = 0;
-	index = -1;
-	while (i <= ft_strlen(s))
-	{
-		if (s[i] != c && index < 0)
-			index = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
-		{
-			split[j++] = duplicate_word(s, index, i);
-			index = -1;
-		}
-		i++;
-	}
-	split[j] = 0;
-	return (split);
 }
