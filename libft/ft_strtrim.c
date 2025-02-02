5aca9b8ae229fe6279fbe556a87230e6a4b917a6
it-runner <144400811+it-runner@users.noreply.github.com>
2025-02-02 21:20:19 +0100

    Add files via upload

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -10,10 +10,9 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*new_str;
 	size_t	i;
-	size_t	end;
 	size_t	len;
+	char	*new_str;
 
 	if (!s1 || !set)
 		return (NULL);
@@ -21,10 +20,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	len = ft_strlen(s1);
 	while (i < len && ft_strchr(set, s1[i]))
 		i++;
-	end = len;
-	while (end > i && ft_strchr(set, s1[end - 1]))
-		end--;
-	len = end - i;
+	while (len > i && ft_strchr(set, s1[len - 1]))
+		len--;
+	len = len - i;
 	new_str = malloc(len + 1);
 	if (!new_str)
 		return (NULL);
